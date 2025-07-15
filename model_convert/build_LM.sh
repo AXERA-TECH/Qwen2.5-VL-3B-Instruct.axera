0f54475e16da20a07ec505affe4792f8ce4946d0
lihongjie <lihongjie@axera-tech.com>
2025-07-15 19:35:20 +0800

    支持axcl

--- model_convert/build_LM.sh
+++ model_convert/build_LM.sh
@@ -1,1 +1,14 @@
-pulsar2 llm_build --input_path ~/AI-support/Qwen/Qwen2.5-VL-3B-Instruct/ --output_path ~/AI-support/Qwen/Qwen2.5-VL-3B-Instruct-AX650-mrope/ --kv_cache_len 1023 --hidden_state_type bf16 --prefill_len 320 --parallel 32 --chip AX650
+set -e 
+
+pulsar2 llm_build --input_path ~/AI-support/Qwen/Qwen2.5-VL-3B-Instruct/ \
+                --output_path ~/AI-support/Qwen/Qwen2.5-VL-3B-Instruct-AX650-chunk_prefill_512/ \
+                --kv_cache_len 1023 \
+                --hidden_state_type bf16 \
+                --prefill_len 128 \
+                --last_kv_cache_len 128 \
+                --last_kv_cache_len 256 \
+                --last_kv_cache_len 384 \
+                --last_kv_cache_len 512 \
+                --parallel 32 --chip AX650
+
+./tools/embed_process.sh ~/AI-support/Qwen/Qwen2.5-VL-3B-Instruct/ ~/AI-support/Qwen/Qwen2.5-VL-3B-Instruct-AX650-chunk_prefill_512/
